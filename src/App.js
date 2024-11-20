c5382b65f09ceae0ff6aaa219164565b58fac5cf
iiiiirisa <tusen.takk39@gmail.com>
2024-11-20 20:42:10 +0900

    html.css commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <html lang="ja">
+      <meta charset="UTF-8" />
+      <header className="header">
+        <div className="header-list">Blog</div>
+        <div className="header-list">お問い合わせ</div>
       </header>
-    </div>
+
+      <body>
+        <div className="post">
+          <div className="info">
+            <p className="date">2023-09-11</p>
+            <ul className="categories">
+              <li>React</li>
+              <li>TypeScript</li>
+            </ul>
+          </div>
+          <h2>APIで取得した記事タイトル１</h2>
+          <p className="content">
+            本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+          </p>
+        </div>
+
+        <div className="post">
+          <div className="info">
+            <p className="date">2023-09-10</p>
+            <ul className="categories">
+              <li>HTML</li>
+              <li>CSS</li>
+            </ul>
+          </div>
+          <h2>APIで取得した記事タイトル２</h2>
+          <p className="content">
+            本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+          </p>
+        </div>
+
+        <div className="post">
+          <div className="info">
+            <p className="date">2023-09-09</p>
+            <ul className="categories">
+              <li>JavaScript</li>
+            </ul>
+          </div>
+          <h2>APIで取得した記事タイトル３</h2>
+          <p className="content">
+            本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+            本文です。本文です。本文です。本文です。本文です。本文です。
+            <br />
+          </p>
+        </div>
+      </body>
+    </html>
   );
 }
 
