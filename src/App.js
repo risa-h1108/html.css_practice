994b066753d1cfda79f5f6c8eb8aa23e061d1151
iiiiirisa <tusen.takk39@gmail.com>
2024-11-23 22:04:29 +0900

    課題1

--- src/App.js
+++ src/App.js
@@ -1,73 +1,14 @@
 import { Header } from "./pages/Header.js";
 import { posts } from "./data/posts.js";
+import { Data } from "./data/index.jsx";
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Header />
+
       <body>
-        <div className="post">
-          <div className="info">
-            <p className="date">2023-09-11</p>
-            <ul className="categories">
-              <li>React</li>
-              <li>TypeScript</li>
-            </ul>
-          </div>
-          <h2>APIで取得した記事タイトル１</h2>
-          <p className="content">
-            本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-          </p>
-        </div>
-
-        <div className="post">
-          <div className="info">
-            <p className="date">2023-09-10</p>
-            <ul className="categories">
-              <li>HTML</li>
-              <li>CSS</li>
-            </ul>
-          </div>
-          <h2>APIで取得した記事タイトル２</h2>
-          <p className="content">
-            本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-          </p>
-        </div>
-
-        <div className="post">
-          <div className="info">
-            <p className="date">2023-09-09</p>
-            <ul className="categories">
-              <li>JavaScript</li>
-            </ul>
-          </div>
-          <h2>APIで取得した記事タイトル３</h2>
-          <p className="content">
-            本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-            本文です。本文です。本文です。本文です。本文です。本文です。
-            <br />
-          </p>
-        </div>
+        <Data />
       </body>
     </>
   );
