8b88bda84ca29b37ff52afa9665836d38b16bf3c
iiiiirisa <tusen.takk39@gmail.com>
2024-11-25 21:34:36 +0900

    課題2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Header } from "./pages/Header.js";
-import { posts } from "./data/posts.js";
 import { Home } from "./data/Home.index.jsx";
 import { Detail } from "./data/Detail.index.jsx";
 import { Route, Routes } from "react-router-dom";
@@ -12,13 +11,12 @@ export const App = () => {
       {/*element属性は、そのパスがマッチしたときに表示するコンポーネントを指定する。*/}
       {/* path="/posts/:id"は、`/posts/1`や`/posts/abc`のように、`/posts/の後に任意の文字列が続くパスを意味する。
         　　ここで:id`はパラメータとして扱われ、その部分にどんな文字列が来てもマッチする */}
-      <body>
-        <Routes>
-          <Route path="/" element={<Home />} />
 
-          <Route path="/posts/:id" element={<Detail />} />
-        </Routes>
-      </body>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/post/:id" element={<Detail />} />
+      </Routes>
     </>
   );
 };
--- src/data/Detail.index.jsx
+++ src/data/Detail.index.jsx
@@ -4,6 +4,7 @@ import { posts } from "./posts";
 import classes from "../styles/Detail.module.css";
 
 export const Detail = () => {
+  console.log(classes);
   const { id } = useParams(); // urlからidを取得。http://localhost:3000/posts/1 というURLだったら、1を取得
   const post = posts.find((post) => post.id === Number(id)); // findメソッドを用いて、posts配列の中から、postのidが、上記で取得したid(7行目のid)と一致するpostオブジェクトを探す。なければundefinedがpostに格納される。
   if (!post)
@@ -12,16 +13,15 @@ export const Detail = () => {
     ); // もし上記のfindメソッドでpostにundefinedが入ったら、!post(!は論理否定)がtrueとなり、「記事が見つかりませんでした」という表示をさせる
 
   return (
-    <>
+    <div className={classes.wrapper}>
+      <img src={post.thumbnailUrl} alt="sampleImage" />
       <div className={classes.info}>
-        <p className={classes.date}>
+        <div className={classes.date}>
           {new Date(post.createdAt).toLocaleDateString()}
-        </p>
+        </div>
         <ul className={classes.categories}>
           {post.categories.map((category, id) => (
-            <li key={id} className={classes.categories}>
-              {category}
-            </li>
+            <li key={id}>{category}</li>
           ))}
         </ul>
       </div>
@@ -30,6 +30,6 @@ export const Detail = () => {
         className={classes.content}
         dangerouslySetInnerHTML={{ __html: post.content }}
       />
-    </>
+    </div>
   );
 };
