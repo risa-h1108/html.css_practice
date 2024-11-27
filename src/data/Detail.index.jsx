0626bfe5e8d33e55d1d536e96700fa7e468cbcde
iiiiirisa <tusen.takk39@gmail.com>
2024-11-27 22:22:55 +0900

    課題3

--- src/data/Detail.index.jsx
+++ src/data/Detail.index.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"; //[useParams]という関数を利用することで、パラメータを取得することが可能になる。
-import { posts } from "./posts";
 import classes from "../styles/Detail.module.css";
 
 export const Detail = () => {
-  console.log(classes);
   const { id } = useParams(); // urlからidを取得。http://localhost:3000/posts/1 というURLだったら、1を取得
-  const post = posts.find((post) => post.id === Number(id)); // findメソッドを用いて、posts配列の中から、postのidが、上記で取得したid(7行目のid)と一致するpostオブジェクトを探す。なければundefinedがpostに格納される。
-  if (!post)
+  const [post, setPost] = useState(null); //post:記事のデータを保存するための状態変数. setPostは`post`の値を更新するための関数.
+  const [loading, setLoading] = useState(false); //loading:データの読み込み中かどうかを示す状態変数. setLoading:`loading`の値を更新するための関数
+
+  // APIでpostsを取得する処理をuseEffectで実行します。
+  useEffect(() => {
+    const fetcher = async () => {
+      setLoading(true); //「今、データを読み込んでいます」ということを示す。
+      const res = await fetch(
+        `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`
+      ); //非同期処理：指定されたURLにHTTPリクエストを送信し、その応答を待つ。
+      const data = await res.json();
+      setPost(data.post);
+      setLoading(false);
+    };
+    fetcher(); //fetcher関数を呼び出して、データの取得を開始.
+  }, []);
+
+  if (loading) return <div className={classes.postLoading}>読み込み中...</div>;
+  if (!loading && !post)
     return (
       <div className={classes.postError}>記事が見つかりませんでした。</div>
-    ); // もし上記のfindメソッドでpostにundefinedが入ったら、!post(!は論理否定)がtrueとなり、「記事が見つかりませんでした」という表示をさせる
+    ); // もしloadingも返ってこず、postも返ってこない場合、「記事が見つかりませんでした」という表示をさせる
 
   return (
     <div className={classes.wrapper}>
